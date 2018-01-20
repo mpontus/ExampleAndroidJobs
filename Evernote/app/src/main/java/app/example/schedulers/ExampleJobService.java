d57553eb99147865b01d04728ee9d9187805ada7
Mikhail Pontus <m.pontus@gmail.com>
2018-01-21 02:06:29 +0300

    Copy Firebase to Evernote

---
+++ Evernote/app/src/main/java/app/example/schedulers/ExampleJobService.java
@@ -0,0 +1,24 @@
+package app.example.schedulers;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import app.example.schedulers.utils.NotificationUtils;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+/**
+ * Background job which shows a notification
+ */
+public class ExampleJobService extends JobService {
+  @Override
+  public boolean onStartJob(JobParameters job) {
+    NotificationUtils.showNotification(this);
+
+    return false; // Answers the question: "Is there still work going on?"
+  }
+
+  @Override
+  public boolean onStopJob(JobParameters job) {
+    return false; // Answers the question: "Should this job be retried?"
+  }
+}
