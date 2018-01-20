fb88afbc0a6bc4da9843da55a2c73d0eaaa96207
Mikhail Pontus <m.pontus@gmail.com>
2018-01-20 22:06:10 +0300

    Make notification compatible with Android Oreo

--- Firebase/app/src/main/java/app/example/schedulers/utils/NotificationUtils.java
+++ Firebase/app/src/main/java/app/example/schedulers/utils/NotificationUtils.java
@@ -1,10 +1,11 @@
 package app.example.schedulers.utils;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-
 import app.example.schedulers.R;
 
 public class NotificationUtils {
@@ -12,6 +13,22 @@ public class NotificationUtils {
   private static final String CHANNEL_ID = "main";
 
   public static void showNotification(Context context) {
+    NotificationManager notificationManager =
+        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+    if (notificationManager == null) {
+      return;
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel mChannel = new NotificationChannel(
+          CHANNEL_ID,
+          context.getString(R.string.main_notification_channel_name),
+          NotificationManager.IMPORTANCE_HIGH);
+
+      notificationManager.createNotificationChannel(mChannel);
+    }
+
     NotificationCompat.Builder notificationBuilder =
         new NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_background)
@@ -19,11 +36,11 @@ public class NotificationUtils {
             .setContentText("Hello world!")
             .setDefaults(Notification.DEFAULT_VIBRATE);
 
-    NotificationManager notificationManager =
-        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-    if (notificationManager != null) {
-      notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+        && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+      notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
     }
+
+    notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
   }
 }
