df633cd58a256f432ea0ed8c60fedce74baeb09e
Mikhail Pontus <m.pontus@gmail.com>
2018-01-21 18:58:31 +0300

    Implement example for Firebase JobDispatcher

--- app/src/main/java/com/example/schedulers/NotificationJobService.java
+++ app/src/main/java/com/example/schedulers/NotificationJobService.java
@@ -1,9 +1,9 @@
 package com.example.schedulers;
 
-import android.app.job.JobParameters;
-import android.app.job.JobService;
 import android.util.Log;
 import com.example.schedulers.utils.NotificationUtils;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
 
 public class NotificationJobService extends JobService {
 
--- app/src/main/java/com/example/schedulers/utils/SchedulerUtils.java
+++ app/src/main/java/com/example/schedulers/utils/SchedulerUtils.java
@@ -1,39 +1,39 @@
 package com.example.schedulers.utils;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import com.example.schedulers.NotificationJobService;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
 import java.util.concurrent.TimeUnit;
 
 public class SchedulerUtils {
-
-  private static final int NOTIFICATION_JOB_ID = 0;
-  private static final int NOTIFICATION_INTERVAL_MILLIS = (int) TimeUnit.MINUTES.toMillis(15);
+  private static final String NOTIFICATION_JOB_TAG = "notification-job";
+  private static final int REMINDER_INTERVAL_SECONDS = (int) TimeUnit.MINUTES.toSeconds(15);
+  private static final int REMINDER_FLEXTIME_SECONDS = (int) TimeUnit.MINUTES.toSeconds(5);
 
   public static void scheduleJob(Context context) {
-    JobScheduler jobScheduler =
-        (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+    FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
 
-    if (jobScheduler == null) {
-      return;
-    }
-
-    ComponentName jobService = new ComponentName(context, NotificationJobService.class);
-
-    JobInfo jobInfo = new JobInfo.Builder(NOTIFICATION_JOB_ID, jobService)
-        .setPeriodic(NOTIFICATION_INTERVAL_MILLIS)
-        .setPersisted(true)
+    Job job = dispatcher.newJobBuilder()
+        .setService(NotificationJobService.class)
+        .setTag(NOTIFICATION_JOB_TAG)
+        .setRecurring(true)
+        .setLifetime(Lifetime.FOREVER)
+        .setTrigger(Trigger.executionWindow(
+            REMINDER_INTERVAL_SECONDS,
+            REMINDER_INTERVAL_SECONDS + REMINDER_FLEXTIME_SECONDS))
+        .setReplaceCurrent(true)
         .build();
 
-    jobScheduler.schedule(jobInfo);
+    dispatcher.mustSchedule(job);
   }
 
   public static void unscheduleJob(Context context) {
-    JobScheduler jobScheduler =
-        (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+    FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
 
-    jobScheduler.cancel(NOTIFICATION_JOB_ID);
+    dispatcher.cancel(NOTIFICATION_JOB_TAG);
   }
 }
